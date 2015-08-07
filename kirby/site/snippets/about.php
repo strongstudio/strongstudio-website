ecba085b1dd5c2b00a77535e7d724061cc505294
andosteinmetz <ando.steinmetz@gmail.com>
2015-08-07 15:00:26 -0400

    minor changes to about page

--- kirby/site/snippets/about.php
+++ kirby/site/snippets/about.php
@@ -19,7 +19,7 @@
                     <div class="staff matthew">
                         <header>
                             <h3>Matthew Strong</h3>
-                            <span class="title">creative director</span>
+                            <span class="title">principal | creative director</span>
                         </header>
                         <p>Matthew founded Strong Studio in 2006. Prior to establishing the company, he served as the Senior Creative Director for Madison Square Garden, overseeing and creating season identities for the New York Knicks and New York Rangers as well as designs for Radio City Music Hall. He began his professional career at SME Branding where he helped develop and extend brands for the NFL, NBA and MLB.</p>
                         <p>In 1999, Matthew graduated from Pratt Institute with highest honors from the Communications Design department where he has been teaching Package Design and Graphic Design since 2004. Matthew is also a member of the American Institute of Graphic Arts (AIGA).</p>
@@ -29,7 +29,7 @@
                     <div class="staff linda">
                         <header>
                             <h3>Linda Sue Strong</h3>
-                            <span class="title">project manager + copywriter</span>
+                            <span class="title">brains of the operation</span>
                         </header>
                         <p>Linda is responsible for the day-to-day project and budget management, working with the design team on brand consistency and workflow. Linda is also Strong Studio's in-house copywriter, creating headlines and establishing brand voices for several of Strong Studio's clients including The New York Times and Benjamin Moore.</p>
                         <p>A graduate of The Cooper Union for the Advancement of Science and Art, Linda began her career designing print and environmental graphics for the arts and entertainment industry including The New York Philharmonic, Carnegie Hall, and American Theatre. Linda later obtained her law degree from the Benjamin N. Cardozo School of Law where she specialized in intellectual property law. After several years as a litigator, she returned to the design industry to help manage and grow Strong Studio.</p>
@@ -46,32 +46,32 @@
                     <div class="staff kate">
                         <header>
                             <h3>Kate</h3>
-                            <span class="title">designer | bookworm</span>
+                            <span class="title">designer | cat collector</span>
                         </header>
                         <p>Cats, Cats, Cats, Cats, Cats, Cats, The Hunger Games, Cats, Cats, Cats, Cats, Cats, Cats, Cats, Cats, Cats, Cats, The Hunger Games, Cats, Cats, Cats, Cats.</p>
+                    </div>
+                    <div class="staff sam">
+                        <header>
+                            <h3>Sam</h3>
+                            <span class="title">designer extraordinaire</span>
+                        </header>
+                        <p>Sam arrived in NYC via Michigan. He has an affinity for hip hop, pop punk, fancy socks, comic books and can out-quote you on "The Office." That's what she said.</p>
+                    </div>
+                </div>
+                <div class="col one-of-two">
+                    <div class="staff caitlyn">
+                        <header>
+                            <h3>Caitlyn</h3>
+                            <span class="title">designer | studio agent</span>
+                        </header>
+                        <p>A City Slicker by way of Texas, Caitlyn enjoys the simple things in life including fire-escapes, knitting, heavy blues music, and long walks down Atlantic Avenue, y'all.</p>
                     </div>
                     <div class="staff alyssa">
                         <header>
                             <h3>Alyssa</h3>
-                            <span class="title">designer | laser tag enthusiast</span>
+                            <span class="title">designer of things</span>
                         </header>
                         <p>When not feeding her love for design, Alyssa enjoys exploring NY secrets, frequenting local concerts, and eating diner omelettes.</p>
-                    </div>
-                </div>
-                <div class="col one-of-two">
-                    <div class="staff sam">
-                        <header>
-                            <h3>Sam</h3>
-                            <span class="title">designer | aspiring improv star</span>
-                        </header>
-                        <p>Sam arrived in NYC via Michigan. He has an affinity for hip hop, pop punk, fancy socks, comic books and can out-quote you on "The Office." That's what she said.</p>
-                    </div>
-                    <div class="staff caitlyn">
-                        <header>
-                            <h3>Caitlyn</h3>
-                            <span class="title">designer | NEED COPY</span>
-                        </header>
-                        <p>Need Copy</p>
                     </div>
                 </div>
             </div>
